eb834c5f13c99d33f0debd95c22066c8609c8044
annamariabukalska <56019427+annamariabukalska@users.noreply.github.com>
2021-05-09 21:34:20 +0200

    Adds Image

--- src/pages/Recipe.js
+++ src/pages/Recipe.js
@@ -1,14 +1,13 @@
-// import logo from './logo.svg';
-//import './App.css';
 import Footer from '../components/Footer/Footer';
 import NavBar from '../components/NavBar/NavBar';
+import recipeImage from '../resources/recipe-example.jpg';
 
 function Recipe() {
   return (
     <main>
     <NavBar />
     <p>Oppskriftssiden</p>
-    
+    <img src={recipeImage} alt="Oppskrift"/>
     <Footer />
     </main>
   );
