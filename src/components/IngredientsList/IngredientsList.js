145379a763aefc54d2a5d444e6d6088aa45dfef5
annamariabukalska <56019427+annamariabukalska@users.noreply.github.com>
2021-05-11 22:59:40 +0200

    IngredientsList wip

---
+++ src/components/IngredientsList/IngredientsList.js
@@ -0,0 +1,15 @@
+import './IngredientsList.css';
+
+function IngredientsList(props) {
+    return (
+      <div className="ingredients-list">
+          <ul>
+              <li>2 egg</li>
+              <li>Sukker</li>
+              <li>Eddik</li>
+          </ul>
+      </div>
+    );
+}
+  
+export default IngredientsList;  
--- src/pages/RecipePage.js
+++ src/pages/RecipePage.js
@@ -1,20 +1,23 @@
-import Footer from '../components/Footer/Footer';
-import './RecipePage.css';
+  import Footer from '../components/Footer/Footer';
+import IngredientsList from '../components/IngredientsList/IngredientsList';
+  import './RecipePage.css';
 
-function RecipePage() {
-  return (
-    <main>
-    <img src="images/recipe-example.jpg" alt="Oppskrift" className="recipe-large-image"/>
-    <div className="content-inset">
-      <h1>Pavlova</h1>
-      <h2>⏰ 3 timer</h2>
+  function RecipePage() {
+    return (
+      <main>
+      <img src="images/recipe-example.jpg" alt="Oppskrift" className="recipe-large-image"/>
+      <div className="content-inset">
+        <h1>Pavlova</h1>
+        <h2>⏰ 3 timer</h2>
 
-<p>Hentet fra Godt.no</p>
-<p>Forvarm ovnen til 180 grader. Bruk en kjøkkenmaskin og pisk eggehvitene med litt av sukkeret til myk skummende marengs ved middels hastighet. Bland maisenna sammen med resten av sukkeret og tilsett sukkeret i marengsen litt etter litt, øk hastigheten når alt sukkeret er tilsatt og pisk til tykk, blank og stiv marengs. Tilsett eddik og visp 10 sekunder til.</p>
-    </div>
-    <Footer />
-    </main>
-  );
-}
+  <p>Hentet fra Godt.no</p>
+  <p>Forvarm ovnen til 180 grader. Bruk en kjøkkenmaskin og pisk eggehvitene med litt av sukkeret til myk skummende marengs ved middels hastighet. Bland maisenna sammen med resten av sukkeret og tilsett sukkeret i marengsen litt etter litt, øk hastigheten når alt sukkeret er tilsatt og pisk til tykk, blank og stiv marengs. Tilsett eddik og visp 10 sekunder til.</p>
 
-export default RecipePage;
+      <IngredientsList />
+      </div>
+      <Footer />
+      </main>
+    );
+  }
+
+  export default RecipePage;
